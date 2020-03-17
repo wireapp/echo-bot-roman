35eb2996ac91c91e6dc1a8e2587dee0423512c2d
Lukas Forst <lukas@forst.pw>
2020-03-17 11:37:00 +0100

    add new API handling by echo bot

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 docker-build:
 	docker build -t lukaswire/echo-bot-roman .
 
-docker-run:
+docker-run: docker-build
 	docker run --rm -p 8080:8080 lukaswire/echo-bot-roman
 
-docker-deploy:
+docker-deploy: docker-build
 	docker push lukaswire/echo-bot-roman
--- client/ResponseHandler.py
+++ client/ResponseHandler.py
@@ -1,10 +1,56 @@
 from api.Configuration import Config
+from client.RomanClient import RomanClient
 
 
 class ResponseHandler:
+    __prefix = 'You said: '
+    __prefix_offset = len(__prefix)
 
     def __init__(self, config: Config):
-        self.confg = config
+        self.config = config
+        self.client = RomanClient(config)
 
-    def handle(self, json: dict):
-        pass
+    def handle_json(self, json: dict):
+        """
+        Handle message received from the Roman
+        """
+        message_type = json['type']
+        try:
+            {
+                'conversation.bot_request': lambda x: print('Handling bot request.'),
+                'conversation.init': self.__init,
+                'conversation.new_text': self.__new_message
+            }[message_type](json)
+        except KeyError:
+            # type is different
+            print(f'Unhandled type: {json["type"]}')
+        except Exception as ex:
+            print(ex)
+
+    def __init(self, json: dict):
+        print('init received')
+
+        self.__send_text("Hello! I'm Echo Bot!", [], json['token'])  # TODO get message info
+
+    def __new_message(self, json: dict):
+        print('New text message received.')
+
+        if not json.get('text'):
+            print(f'Unsupported payload.')
+            return
+
+        payload = json['text']
+
+        text = payload['data']
+        mentions = payload['mentions']
+
+        new_text = self.__prefix + text
+        for mention in mentions:
+            mention['offset'] = mention['offset'] + self.__prefix_offset
+
+        self.__send_text(new_text, mentions, json['token'])
+
+    def __send_text(self, message: str, mentions: list, token: str):
+        text = {'data': message, 'mentions': mentions}  # TODO add support for more types than just text
+        msg = {'type': 'create', 'text': text}
+        self.client.send_message(token, msg)
