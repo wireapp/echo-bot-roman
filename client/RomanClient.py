270a71235a8e5deb330838c34055153dc72970fc
Lukas Forst <lukas@forst.pw>
2020-06-01 13:56:15 +0200

    add some debug logs

--- client/RomanClient.py
+++ client/RomanClient.py
@@ -13,10 +13,11 @@ class RomanClient:
         self.url = config.roman_url
 
     def send_message(self, token: str, payload: dict) -> str:
-        logger.debug(f'Sending {payload} to roman.')
+        logger.debug(f'Sending {payload} to {self.url}/conversation.')
         r = requests.post(f"{self.url}/conversation", data=json.dumps(payload),
                           headers={"content-type": "application/json", "Authorization": f"Bearer {token}"})
         logger.debug(f'Status code: {r.status_code}')
         j = r.json()
         logger.debug(f'Response: {j}')
+        logger.debug(f'Headers: {r.headers}')
         return j
